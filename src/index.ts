fd62a4dcf72dd442d1dc3196f493e9129b898a23
Denis Makarov <denis.makarov@intravision.ru>
2021-01-03 17:03:10 +0600

    create logout, protected router

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
+import coockieSession from 'cookie-session';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(coockieSession({ keys: ['fdsfds'] }));
 app.use(router);
 
 app.listen(3000, () => {
