38c620403680d543e744e00f99650f0dcfa8c0aa
Denis Makarov <denis.makarov@intravision.ru>
2021-01-04 20:22:24 +0600

    create singleton: AppRouter

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@ import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import coockieSession from 'cookie-session';
-
-import { router as controllerRouter } from './controllers/decorators/controller';
-
+import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
 
 const app = express();
@@ -12,7 +10,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(coockieSession({ keys: ['fdsfds'] }));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listenning on port 3000')
