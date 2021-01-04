b765245d8ea7718dfaae66c4eee10399373ed582
Denis Makarov <denis.makarov@intravision.ru>
2021-01-04 18:44:46 +0600

    create decorators for login route

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,16 @@ import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import coockieSession from 'cookie-session';
 
+import { router as controllerRouter } from './controllers/decorators/controller';
+
+import './controllers/LoginController';
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(coockieSession({ keys: ['fdsfds'] }));
 app.use(router);
+app.use(controllerRouter);
 
 app.listen(3000, () => {
   console.log('Listenning on port 3000')
