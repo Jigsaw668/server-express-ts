29a7610ae968a5082bcca69085322205a7e5bdb5
Denis Makarov <denis.makarov@intravision.ru>
2021-01-05 13:35:44 +0600

    fixing routes

--- src/controllers/decorators/index.ts
+++ src/controllers/decorators/index.ts
@@ -1,2 +1,5 @@
 export * from './controller';
 export * from './routes';
+export * from './use';
+export * from './bodyValidator';
+
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import express, { Request, Response } from 'express';
-import { router } from './routes/loginRoutes';
 import bodyParser from 'body-parser';
 import coockieSession from 'cookie-session';
 import { AppRouter } from './AppRouter';
 import './controllers/LoginController';
+import './controllers/RootController';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(coockieSession({ keys: ['fdsfds'] }));
-app.use(router);
 app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
